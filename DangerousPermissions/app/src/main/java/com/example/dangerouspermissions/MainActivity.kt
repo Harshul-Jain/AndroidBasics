88d8a3b62920ea9e0921d106a452d22d01f322b9
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-07-13 20:15:31 +0530

    SecurityException Again

--- DangerousPermissions/app/src/main/java/com/example/dangerouspermissions/MainActivity.kt
+++ DangerousPermissions/app/src/main/java/com/example/dangerouspermissions/MainActivity.kt
@@ -16,8 +16,8 @@ class MainActivity : AppCompatActivity() {
             val i=Intent(Intent.ACTION_VIEW,uri)//ACTION_VIEW and ACTION_DIAL will show Dial page
             // but ACTION_CALL will directly CALL
             startActivity(i)
-            //the App crashes because of Security Exception
-            //so we will add permission in Android Manifest
+            //the App crashes because of Security Exception again
+            //because the permission is dangerous permission so need user permission to access
         }
     }
 }
