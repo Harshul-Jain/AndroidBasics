f3bae331d80125bc2ae66a4d1d870e1ba2fb21a5
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-07-18 10:01:31 +0530

    RecyclerView

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/Fruit.kt
@@ -0,0 +1,38 @@
+package com.example.recyclerview
+
+import kotlin.random.Random
+
+data class Fruit(
+    val name: String,
+    val origin: String,
+    val quantity: Int
+) {
+    companion object {
+
+        @JvmField
+        val FRUIT_NAMES = arrayOf(
+            "Apple", "Mango", "Pineapple", "Strawberry", "Guava",
+            "Grapes", "Kiwi", "Banana", "Watermelon", "Orange"
+        )
+
+        @JvmField
+        val ORIGINS = arrayOf(
+            "Lucknow", "Himachal", "Patna", "Nagpur", "Mysore", "Srinagar", "Tamil Nadu"
+        )
+
+        @JvmStatic
+        fun getRandomFruits(n: Int): ArrayList<Fruit> {
+            val fruitArray = ArrayList<Fruit>(n)
+            for (i in 1..n) {
+                fruitArray.add(
+                    Fruit(
+                        FRUIT_NAMES[Random.nextInt(10)],
+                        ORIGINS[Random.nextInt(7)],
+                        Random.nextInt(10) * 100
+                    )
+                )
+            }
+            return fruitArray
+        }
+    }
+}
