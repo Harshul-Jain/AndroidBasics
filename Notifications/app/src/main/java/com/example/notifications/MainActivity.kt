48d655bf0c381601f4dd75f31f664f6dce5b4f95
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-07-29 17:47:09 +0530

    Clickable Notifications

--- Notifications/app/src/main/java/com/example/notifications/MainActivity.kt
+++ Notifications/app/src/main/java/com/example/notifications/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.notifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
             )
         }
         val button: Button = findViewById(R.id.button)
+        val button1: Button = findViewById(R.id.button1)
         button.setOnClickListener {
             val simpleNotification = NotificationCompat.Builder(this, "first")
                 .setContentTitle("Simple Title")
@@ -35,6 +39,22 @@ class MainActivity : AppCompatActivity() {
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .build()
             nm.notify(1, simpleNotification)
+        }
+        button1.setOnClickListener {
+            val i = Intent()
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("https://www.google.com")
+
+            val pi = PendingIntent.getActivity(this, 123, i, PendingIntent.FLAG_UPDATE_CURRENT)
+            val clickableNotification = NotificationCompat.Builder(this, "first")
+                .setContentTitle("Simple Title")
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .setContentText("This is sample description of the notification")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build()
+            nm.notify(2, clickableNotification)
         }
     }
 }
