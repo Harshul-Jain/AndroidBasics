34e299f3734a50294bd842ab4eff4848754baf86
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-08-05 03:45:35 +0530

    Intro to Room Database and Dao's

---
+++ RoomDB/app/src/main/java/com/example/roomdb/UserDao.kt
@@ -0,0 +1,21 @@
+package com.example.roomdb
+
+import androidx.room.*
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(user: User)
+
+    @Insert
+    fun insertAll(list: List<User>)
+
+    @Delete
+    fun delete(user: User)
+    @Query("SELECT * FROM User")
+    fun getAllUser():List<User>
+
+    @Query("Select * From User where age>=:age")
+    fun getUserWithAge(age:Int):List<User>
+
+}
