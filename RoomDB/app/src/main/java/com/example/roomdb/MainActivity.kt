37f140826b7851a5253b4b68eecad1a45b01c0c2
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-08-05 04:47:51 +0530

    Populating Views from database

--- RoomDB/app/src/main/java/com/example/roomdb/MainActivity.kt
+++ RoomDB/app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +21,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        button.setOnClickListener {
+            db.userDao().insert(User("Harshul", "9868837145", "Delhi", 20))
+
+        }
+        button2.setOnClickListener {
+            val list = db.userDao().getAllUser()
+            if (list.isNotEmpty()) {
+                with(list[0]) {
+                    textView.text = name
+                    textView2.text = age.toString()
+                    textView3.text = address
+                    textView4.text = number
+                }
+            }
+        }
     }
 
 }
