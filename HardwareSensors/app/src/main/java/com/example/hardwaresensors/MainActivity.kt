8aaf9d7c10bccd27031c3c96021b0ea2758cefc2
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-08-23 16:45:21 +0530

    SensorEventListener

--- HardwareSensors/app/src/main/java/com/example/hardwaresensors/MainActivity.kt
+++ HardwareSensors/app/src/main/java/com/example/hardwaresensors/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hardwaresensors
 
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,20 +11,37 @@ import android.widget.Toast
 import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sensorEventListener: SensorEventListener
+    lateinit var sensorManager: SensorManager
+    lateinit var proxSensor: Sensor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val sensorManager = getSystemService<SensorManager>()
-        if(sensorManager == null){
-            Toast.makeText(this,"Could not get sensors",Toast.LENGTH_SHORT )
-            finish()
-        }else{
-            val sensors = sensorManager.getSensorList(Sensor.TYPE_ALL)
-            sensors.forEach {
-                Log.d("HWSENS","""
-                    ${it.name} | ${it.stringType} | ${it.vendor} 
-                """.trimIndent())
+        proxSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        sensorEventListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                Log.d("HWSENS", """
+                   onSensorChanged : ${event!!.values[0]}
+               """.trimIndent())
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                //nothing
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(sensorEventListener, proxSensor, 1000 * 1000)
+    }
+
+    override fun onPause() {
+        sensorManager.unregisterListener(sensorEventListener)
+        super.onPause()
     }
 }
