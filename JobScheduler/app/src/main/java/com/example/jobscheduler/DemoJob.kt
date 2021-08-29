81a36729f2e106d7ba44e65e8e3bd2385a314e89
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-08-30 04:18:50 +0530

    Added DemoJob class

--- JobScheduler/app/src/main/java/com/example/jobscheduler/DemoJob.kt
+++ JobScheduler/app/src/main/java/com/example/jobscheduler/DemoJob.kt
@@ -2,11 +2,21 @@ package com.example.jobscheduler
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.widget.Toast
 
 class DemoJob : JobService() {
     override fun onStartJob(params: JobParameters?): Boolean {
+        Toast.makeText(this, "Hello , I'm a scheduled Job", Toast.LENGTH_SHORT).show()
         return false
+        /*
+        Async job
+
+        return true // This true means that there is work still going on, so don't
+        finish this Service just yet
+         */
     }
+
+    
 
     override fun onStopJob(params: JobParameters?): Boolean {
         return false
